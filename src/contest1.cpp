da190d953c070a2d7c7625f6eefad977f05e80c7
Osvald Nitski <47303810+OsvaldN@users.noreply.github.com>
2021-02-09 22:56:49 -0500

    still WIP rotation control

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -11,6 +11,9 @@
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_datatypes.h>
 
+#define MAX_SPINRATE (0.6)
+#define SPEED_LIM (0.25)
+#define OBS_SPEED_LIM (0.1)
 #define N_BUMPER (3)
 #define RAD2DEG(rad) ((rad) *180./M_PI)
 #define DEG2RAD(deg) ((deg) *M_PI /180.)
@@ -34,6 +37,13 @@ int32_t F_bumper_idx =0;
 //variable triggered if one of side bumpers is pressed to start sequence of events
 int32_t L_bumper_idx =0;
 int32_t R_bumper_idx =0;
+
+
+/*
+//////////////////////
+/ Callback functions /
+//////////////////////
+*/
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
@@ -66,9 +76,82 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
     posX = msg->pose.pose.position.x;
     posY = msg->pose.pose.position.y;
     yaw = tf::getYaw(msg->pose.pose.orientation);
-    tf::getYaw(msg->pose.pose.orientation);
-    ROS_INFO("Position: (%f,%f) Orientation: %f rad or %f degrees.", posX, posY,yaw,RAD2DEG(yaw));
+    //tf::getYaw(msg->pose.pose.orientation);
+    //ROS_INFO("Position: (%f,%f) Orientation: %f rad or %f degrees.", posX, posY,yaw,RAD2DEG(yaw));
 }
+
+/*
+////////////////////////
+/ Navigation functions /
+////////////////////////
+*/
+
+int randRange(int low, int high){
+    // Uniform random from interval [low, high]
+    return low + ( rand() * (high-low) );
+}
+
+void VelPub(float angular, float linear, ros::Publisher vel_pub){
+    /*
+    Publish a velocity pair using using vel_pub
+    TODO: make vel_pub global so it doesn't need to be passed?
+     */
+
+    geometry_msgs::Twist vel;
+    vel.angular.z = angular;
+    vel.linear.x = linear;
+    vel_pub.publish(vel);
+
+    return;
+}
+
+void rotByAngle(float angle, ros::Publisher vel_pub){
+    /*
+    Rotates the robot by (angle) degrees
+	while this could be rad the abs() rounds the difference calculation
+	and having a resolution of 1 rad would not be great
+     */
+
+    // Rotate at maximum speed in direction of angle
+    float dir = (angle > 0) - (angle < 0);
+    float rotVel = dir * MAX_SPINRATE;
+
+    ros::spinOnce(); //TODO: if spin stays in main loop maybe remove from here?
+    float startYaw = yaw;
+
+    // start spinning
+    VelPub(rotVel, 0.0, vel_pub);
+    ROS_INFO("Set rotation velocity: %f", rotVel);
+    ROS_INFO("Start Orientation: %f rad or %f degrees.",yaw,RAD2DEG(yaw));
+    // this might have a problem at wrap-around if switching between -pi and pi
+    while ((yaw - startYaw) < angle) {    
+        VelPub(rotVel, 0.0, vel_pub);
+	ros::spinOnce();
+	//ROS_INFO("Mid-spin: %f rad, diff: %f, angle: %f, started at: %f",yaw,yaw- startYaw,angle, startYaw);
+    	//exit(0);
+    }
+
+    // stop rotating
+    VelPub(0.0, 0.0, vel_pub);
+
+    return;
+}
+
+void spinAndStep() {
+    /*
+    Spin 360 then step in direction of most open space
+     */
+
+    //pseudo-code
+    
+    return;
+}
+
+/*
+////////
+/ main /
+////////
+*/
 
 int main(int argc, char **argv)
 {
@@ -90,112 +173,28 @@ int main(int argc, char **argv)
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
+    
+    // random seed
+    //srand(0)
 
     while(ros::ok() && secondsElapsed <= 900) {
         ros::spinOnce();
-        //
 
-        ROS_INFO("Position: (%f,%f) Orientation: %f degrees Range", posX, posY,RAD2DEG(yaw),minLaserDist);
+        rotByAngle(0.1, vel_pub);
+        rotByAngle(-0.1, vel_pub);
 
-        bool any_bumper_pressed=false;
+        // remove??
+        //vel.angular.z = angular;
+        //vel.linear.x = linear;
+        //vel_pub.publish(vel);
 
-        angular = 0.0;
-        linear = 0.2;
+        // TODO: display type of motion taking place in current loop
 
-        //check to see if any of the bumpers are being pressed
-        for(uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
-            any_bumper_pressed|=(bumper[b_idx] ==kobuki_msgs::BumperEvent::PRESSED);
-        }
-        //
-        // Control logic after bumpers are being pressed.
-
-        //if the front bumper is pressed reverse and turn following sequence of events
-    
-        if (F_bumper_idx >= 1 && F_bumper_idx < 5){
-            angular =0.0;
-            linear =-0.2;
-            F_bumper_idx += 1;
-
-        }
-        else if (F_bumper_idx >= 1 && F_bumper_idx < 40){
-            angular = M_PI /6;
-            linear = 0.0;
-            F_bumper_idx += 1;
-
-        }
-        else if (F_bumper_idx >= 1){
-            F_bumper_idx = 0;
-
-        }
-        // If side bumpers are pressed turn away from the wall
-
-        if (R_bumper_idx >= 1 && R_bumper_idx < 12){
-            angular = M_PI/6;
-            linear = 0.0;
-            R_bumper_idx += 1;
-
-        }
-        else if (R_bumper_idx >= 1){
-            R_bumper_idx = 0;
-
-        }
-
-        if (L_bumper_idx >= 1 && L_bumper_idx < 12){
-            angular = -M_PI/6;
-            linear = 0.0;
-            L_bumper_idx += 1;
-
-        }
-        else if (L_bumper_idx >= 1){
-            L_bumper_idx = 0;
-
-        }
-
-
-        if (bumper[1] ==kobuki_msgs::BumperEvent::PRESSED) {
-            F_bumper_idx = 1;
-        }
-        //if right bumper is pressed turn to the left
-        else if (bumper[2] ==kobuki_msgs::BumperEvent::PRESSED) {
-            R_bumper_idx = 1;
-        }
-        //if left bumper is pressed turn to the right
-        else if (bumper[0] ==kobuki_msgs::BumperEvent::PRESSED) {
-            L_bumper_idx = 1;
-        }
+        // display motion info
+        ROS_INFO("Linear  Velocity: %f", linear);
+        ROS_INFO("Angular Velocity: %f", angular);
+        ROS_INFO("Position: (%f,%f) Orientation: %f degrees Range: %f", posX, posY,RAD2DEG(yaw),minLaserDist);
         
-        /*
-        //if there is space in front and the bumpers are not pressed go forward
-        else if (minLaserDist>1. && !any_bumper_pressed) {
-            linear = 0.1;
-
-            //correct robot's trajectory so that its x position is 0.5 andits orientation is 90 degrees
-            if (yaw <17 / 36*M_PI || posX>0.6) {
-                angular = -M_PI/12.;
-            }
-            else if (yaw <19 / 36*M_PI || posX<0.4) {
-                angular = -M_PI/12.;
-            }
-            else {
-                angular = 0;
-            }
-        }
-        else {
-            angular =0.0;
-            linear =0.0;
-            break;
-        }
-        */
-
-        vel.angular.z = angular;
-        vel.linear.x = linear;
-        vel_pub.publish(vel);
-
-        // "XXXXX you can print stuff here for debugging XXXXX"
-        std::cout << "XXXXX you can print stuff here for debugging XXXXX" << std::endl;
-        std::cout << "XXXXX you can print stuff here for debugging XXXXX" << std::endl;
-        std::cout << "XXXXX you can print stuff here for debugging XXXXX" << std::endl;
-
         // The last thing to do is to update the timer.
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
         loop_rate.sleep();
