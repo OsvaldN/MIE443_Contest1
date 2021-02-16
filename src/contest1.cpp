3686cf7e80c9cd5fa66ad31e2a54d3c01b3a20fb
David-Rolko <77452568+David-Rolko@users.noreply.github.com>
2021-02-16 16:15:20 -0500

    Laser Callback setup
    Laser Callback copies the messages from the laser into global variables
    A minimum distance function was setup
    a function to convert an angle relative to the robot to the yaw angle was setup

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -30,15 +30,13 @@ float posX = 0.0, posY = 0.0, yaw = 0.0;
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 
 //laser variables
-float minLaserDist = std::numeric_limits<float>::infinity();
-int32_t nLasers=0, desiredNLasers=0, desiredAngle=5;
 
-//variable triggered if front bumper pressed to start sequence of events
-int32_t F_bumper_idx =0;
-//variable triggered if one of side bumpers is pressed to start sequence of events
-int32_t L_bumper_idx =0;
-int32_t R_bumper_idx =0;
+int32_t nLasers=0;
+float laser_min_ang = 0, laser_max_ang =0, laser_ang_increment =0;
+float LaserArray[639]; // unfortunately we have to hard code this size, unless we try to use malloc on the array or use a vector instead
+float LaserArrayAng[639];
 
+float minDistanceDir = 0.0; // direction of the minimum distance
 
 /*
 //////////////////////
@@ -54,23 +52,41 @@ void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	minLaserDist = std::numeric_limits<float>::infinity();
+    //obtain the size of the message array recieved from the laser (number of lasers)
     nLasers = (msg->angle_max - msg->angle_min)/msg->angle_increment;
-    desiredNLasers = desiredAngle*M_PI/(180*msg->angle_increment);
-    // TODO: consider removing this, it gets printed a lot, is it informative?
-    ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
 
-    if (desiredAngle*M_PI/180 < msg->angle_max && -desiredAngle*M_PI/ 180 > msg->angle_min){
-        for (uint32_t laser_idx = nLasers/2-desiredNLasers; laser_idx < nLasers/2+desiredNLasers; ++laser_idx){
-            minLaserDist = std::min(minLaserDist,msg->ranges[laser_idx]);
-        }
-    }
-    else {
-        for(uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx){
-            minLaserDist = std::min(minLaserDist,msg->ranges[laser_idx]);
+    // copy data so it can be used in other sets of code
+    laser_min_ang = msg->angle_min;
+    laser_max_ang = msg->angle_max;
+    laser_ang_increment = msg->angle_increment;
 
+    //return an array with the angle of each laser with the same indexing as LaserArray
+
+    if (LaserArrayAng[0] == 0){
+
+        float angle_temp = laser_min_ang;
+
+        for (uint32_t angle_idx = 0; angle_idx < nLasers; ++angle_idx){
+            LaserArrayAng[angle_idx] = angle_temp;
+            angle_temp = angle_temp + laser_ang_increment;
         }
+
     }
+
+    //copy the ranges array to LaserArray
+    //Note: the the laser scans from right to left so LaserArray[0] is the distance all the way to the right of FOV
+
+    for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx){
+        LaserArray[laser_idx] = msg->ranges[laser_idx];
+        //ROS_INFO("LaserArray element %i is: %f", laser_idx, LaserArray[laser_idx]);
+
+    }
+
+    //msg->angle_max = max angle of the sensor (0.5242763 rad or 29.88 deg)
+    //msg->angle_min = minimum angle of the sensor (-0.5215679 rad or 30.04 deg)
+    //msg->angle_increment = the resolution of the sensor in radians (0.001636689 rad or 0.09434 deg)
+    //msg->ranges[] = the array of distances
+    
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -87,7 +103,9 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 */
 
 // TODO: test this function out, not sure if it works properly
-int randRange(float low, float high){
+// random number generator
+
+int randRange(float low, float high){ //should this not a return a float not an integer?
     // Uniform random from interval [low, high]
     return low + ( (rand() / double(RAND_MAX)) * (high-low) );
 }
@@ -179,6 +197,80 @@ void stepDistance(float distance, float speed,ros::Publisher *vel_pub, bool verb
     return;
 }
 
+float minDistance(int32_t desiredAngle, bool verbose=true){
+    //returns the minimum distance in a view of the desired angle in DEGREES on either side
+    
+    //set the minimum distance to a very large value where a new smaller value can be found
+    float minLaserDist = std::numeric_limits<float>::infinity();
+    int32_t  desiredNLasers=0;
+    minDistanceDir = 0.0; // store the direction of minimum direction into this global variable
+    //determine the number of laser needed to scan the desired angle.  Note: convert desired angle from degrees to radians
+    desiredNLasers = desiredAngle*M_PI/(180*laser_ang_increment);
+
+    //search the range from the negative and positve desired angle (converted to radians)
+    if (desiredAngle*M_PI/180 < laser_max_ang && -desiredAngle*M_PI/ 180 > laser_min_ang){
+   
+        for (uint32_t laser_idx = nLasers/2-desiredNLasers; laser_idx < nLasers/2+desiredNLasers; ++laser_idx){
+
+            if (LaserArray[laser_idx] < minLaserDist){
+                minDistanceDir = LaserArrayAng[laser_idx];
+            }
+            minLaserDist = std::min(minLaserDist, LaserArray[laser_idx]);
+            
+        }
+
+        if (verbose){
+            ROS_INFO("Search in range: %i to %i degrees with %i number of lasers", -desiredAngle, desiredAngle, 2*desiredNLasers);
+            ROS_INFO("minLaserDist measured is: %f and it direction relative to the robot is %f CCW", minLaserDist, minDistanceDir);
+        }
+
+    }
+    //if the desired angle is larger than the sensor field, use the maximum angle
+    else {
+        for(uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx){
+
+            if (LaserArray[laser_idx] < minLaserDist){
+                minDistanceDir = LaserArrayAng[laser_idx];
+            }
+            minLaserDist = std::min(minLaserDist, LaserArray[laser_idx]);
+
+        }
+
+        if (verbose){
+            ROS_INFO("Search max range of laser with max number of lasers which is %i", nLasers);
+            ROS_INFO("minLaserDist measured is: %f and it direction relative to the robot is %f CCW", minLaserDist, minDistanceDir);
+        }
+
+    }
+
+    return minLaserDist;
+}
+
+float GetYawAngle(float input_angle, bool verbose=true){
+
+    //input the angle relative to the robot (eg. LaserArrayAng[0] which is -0.52rad CCW offset (or all the way to the right) from directly in front of the robot)
+    //and return the Yaw angle of that ranging from -pi to pi (in radians)
+
+    float Output_YawAngle = input_angle + yaw;
+
+    //ensure that yaw is within -pi to pi
+    if (Output_YawAngle < -M_PI){
+        Output_YawAngle = Output_YawAngle + (2*M_PI);
+    }
+
+    if (Output_YawAngle > M_PI){
+        Output_YawAngle = Output_YawAngle - (2*M_PI);
+    }
+
+    if (verbose){
+        ROS_INFO("angle relative to robot is: %f the corresponding yaw angle is: %f and the current robot yaw is: %f CCW", input_angle, Output_YawAngle, yaw);
+    }
+
+    return Output_YawAngle;
+}
+
+
+
 void wallFollow(ros::Publisher *vel_pub) {
     /* 
     If robot encouters a wall, make a +CCW rotation and keep wall on RHS. Otherwise proceed
@@ -197,7 +289,11 @@ void wallFollow(ros::Publisher *vel_pub) {
 void spinAndStep(float step) {
     /*
     Spin 360 then step in direction of most open space
-     */
+
+    To prevent going backward maybe only move in a direction in the forawrd facing feild of view (maybe 240 degrees windwow instad of 360)
+    unless it is in a corner and all angles are very small then it should turn around
+
+    */
 
     //code here
     
@@ -207,7 +303,7 @@ void spinAndStep(float step) {
 void spinToDist(float reqDist, float dir) {
     /*
     Rotates in direction (dir) until the laser callback finds an open distance of (reqDist)
-     */
+    */
 
     //spin and scan and then stop once minDist > reqDist
 
@@ -226,6 +322,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
+
     ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
@@ -247,12 +344,22 @@ int main(int argc, char **argv)
     while(ros::ok() && secondsElapsed <= 900) {
         ros::spinOnce();
 
-	// random spin followed by random step
+	    // random spin followed by random step
         //rotByAngle(randRange(-M_PI/2, M_PI/2), &vel_pub);
         //stepDistance(randRange(0.0, 100.0), SPEED_LIM, &vel_pub);
 	
 
         wallFollow(&vel_pub); // DEBUG REMOVE
+        
+        // !!!!!!!!!!! example of David's new code below Feb 16 - Please remove if understood !!!!!!!!!!!
+        minDistance(10); //returns the minimum distance in a range of +- 10 degrees in front of the robot
+        //the direction relative to the robot of the this minimum distance is stored in minDistanceDir
+
+        //returns the Yaw of the angle relative to the robot stared in LaserArrayAng
+        GetYawAngle(LaserArrayAng[0]); //this is all the way to the right of the sensor FOV
+        GetYawAngle(LaserArrayAng[319]); //this is directly ahead of the robot so should match the robots yaw
+        // !!!!!!!!!!! example of David's new code above Feb 16 - Please remove if understood !!!!!!!!!!!!!
+
         /*
         rotByAngle(-M_PI/2, &vel_pub);
         stepDistance(50, SPEED_LIM, &vel_pub);
@@ -265,7 +372,7 @@ int main(int argc, char **argv)
         //          in high-level controller blocks make appropriate print statements
 
         // display motion info
-        ROS_INFO("Position: (%f,%f) Orientation: %f degrees Range: %f", posX, posY,RAD2DEG(yaw),minLaserDist);
+        ROS_INFO("Position: (%f,%f) Orientation: %f degrees", posX, posY,RAD2DEG(yaw));
         
         // The last thing to do is to update the timer.
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
